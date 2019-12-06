17be6ead97426f9538970067fbfcf9d9bb66b134
Kimio Kuramitsu <kkuramitsu@gmail.com>
2019-12-06 18:12:37 +0900

    updated package

--- src/puppy2d.ts
+++ src/puppy2d.ts
@@ -213,24 +213,24 @@ const DefaultPuppyCode: PuppyCode = {
     //   columns: 3,
     //   //part: { shape: 'rectangle' },
     // });
-    world.newObject({
-      shape: 'array',
-      position: new Vector(0, 0),
-      margin: 10,
-      part: { shape: 'circle', restitution: 1.0 },
-    });
-    const sensor: any = world.Rectangle(-200, 200, 260, 260, { frictionAir: 1, isSensor: true, isStatic: true });
-    sensor.moveover = (bodyA: Body, bodyB: Body) => {
-      console.log(bodyA);
-      console.log(bodyB);
-    }
-    world.Variable('TIME', 320, -400, { width: 260 });
-    world.Variable('MOUSE', 320, -440, { width: 260 });
-    for (var i = 0; i < 20; i++) {
-      world.paint(Math.sin(i) * 100, Math.cos(i) * 100, 20);
-      //world.print(`${i}`);
-      yield 200;
-    }
+    // world.newObject({
+    //   shape: 'array',
+    //   position: new Vector(0, 0),
+    //   margin: 10,
+    //   part: { shape: 'circle', restitution: 1.0 },
+    // });
+    // const sensor: any = world.Rectangle(-200, 200, 260, 260, { frictionAir: 1, isSensor: true, isStatic: true });
+    // sensor.moveover = (bodyA: Body, bodyB: Body) => {
+    //   console.log(bodyA);
+    //   console.log(bodyB);
+    // }
+    // world.Variable('TIME', 320, -400, { width: 260 });
+    // world.Variable('MOUSE', 320, -440, { width: 260 });
+    // for (var i = 0; i < 20; i++) {
+    //   world.paint(Math.sin(i) * 100, Math.cos(i) * 100, 20);
+    //   //world.print(`${i}`);
+    //   yield 200;
+    // }
     world.World({ wireframes: true });
     for (var i = 0; i < 40; i++) {
       world.paint(Math.sin(i) * 100, Math.cos(i) * 100, 20);
